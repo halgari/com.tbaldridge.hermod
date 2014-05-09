e5c5827a766224c65a95de7c24c77af10265f492
Timothy Baldridge <tbaldidge@gmail.com>
2014-05-09 11:23:49 -0600

    removed old netty dep

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject com.tbaldridge.hermod "0.1.0-SNAPSHOT"
+(defproject com.tbaldridge.hermod "0.1.2"
   :description "A Mailbox based distributed computing library"
   :url "http://github.com/halgari/com.tbaldridge.hermod"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.fressian "0.2.0"]
-                 [io.netty/netty-all "4.0.11.Final"]
                  [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]])
